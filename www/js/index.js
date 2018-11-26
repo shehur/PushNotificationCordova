fe4ae6589614aeabe59cb586ce66a18542ba5a97
shehur <shehur5727@gmail.com>
2018-11-26 11:36:15 -0200

    update 04

--- www/js/index.js
+++ www/js/index.js
@@ -9,17 +9,31 @@ var app = {
 
     receivedEvent: function(id) {
       var acionar = document.getElementById('acionar');
+      var resposta = document.getElementById('resposta');
+
       acionar.addEventListener('click', function() {
+          resposta.innerText = 'ação executada.';
        		alert('ação executada.');
       });
 
-      window.plugins.OneSignal
-        .startInit("260e2302-5f86-4707-ac95-2f06511e6c1c")
-        .handleNotificationReceived(function(jsonData) {
-          alert("Notificação recebida:\n" + JSON.stringify(jsonData));
-          console.log('Notificação recebida: ' + JSON.stringify(jsonData));
-        })
-        .endInit();
+	   var notificationOpenedCallback = function(jsonData) {
+        var str_json = JSON.stringify(jsonData);
+        resposta.innerText = str_json;
+
+        var json = JSON.parse(str_json);
+
+	   		var Titulo = json.headings.en;
+	   		var Mensagem = json.contents.en;
+
+	   		alert("" + Titulo + "\n" + Mensagem);
+
+	   };
+
+	  window.plugins.OneSignal
+	    .startInit("260e2302-5f86-4707-ac95-2f06511e6c1c")
+	    .handleNotificationOpened(notificationOpenedCallback)
+	    .endInit();       
+    }
 };
 
 app.initialize();
